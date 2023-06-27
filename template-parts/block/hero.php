49370277b78d8767cd9c2a1d168f47441b84562c
Jay WInebrenner <jaywinebrenner@gmail.com>
2023-06-27 14:31:32 -0700

    wire up hero

--- template-parts/block/hero.php
+++ template-parts/block/hero.php
@@ -10,10 +10,23 @@
 $id = 'hero-' . $block['id'];
 ?>
 
-<div id="<?php echo $id; ?>" class="hc-hero" style="background-image: url('<?php the_field('background'); ?>');">
-  <div class="hc-hero__content container">
-    <h5><?php the_field('eyebrow'); ?></h5>
-    <h1><?php the_field('title'); ?></h1>
-    <p><?php the_field('content'); ?></p>
+<section id="<?php echo $id; ?>" class="hero">
+  <div class="hero__top">
+    <div class="hero__content container">
+      <h1><?php the_field('title'); ?></h1>
+      <p><?php the_field('subtitle'); ?></p>
+      <?php
+      $button = get_field('button');
+      if( $button ): ?>
+      <div class="btn-wrapper">
+        <a class="btn"><?php echo $button['text']?></a>
+      </div>
+      <?php endif; ?>
+    </div>
   </div>
-</div>
+  <div class="hero__bottom">
+  <?php if( get_field('image') ): ?>
+    <img src="<?php the_field('image'); ?>" />
+    <?php endif; ?>
+  </div>
+  </section>
