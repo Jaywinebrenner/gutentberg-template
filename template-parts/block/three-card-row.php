ca47c38630e3a9700f49c025caa393ca4ce2a20d
Jay WInebrenner <jaywinebrenner@gmail.com>
2023-06-29 11:18:38 -0700

    dev/three-card-row

--- template-parts/block/three-card-row.php
+++ template-parts/block/three-card-row.php
@@ -12,11 +12,37 @@ $id = 'three-card-row-' . $block['id'];
 
 <section id="<?php echo $id; ?>" class="three-card-row">
   <div class="container">
+
     <div class="three-card-row__top">
-      <h1 class="title"><?php the_field('eyebrow'); ?></h1>
+      <h1 class="title"><?php the_field('title'); ?></h1>
+      <p class="title"><?php the_field('subtitle'); ?></p>
+    </div>
+    <div class="cards-wrapper">
+    <?php if( have_rows('card') ): ?>
+    <?php while( have_rows('card') ): the_row(); 
+
+        // Get sub field values.
+        $title = get_sub_field('title');
+        $copy = get_sub_field('copy');
+        $image = get_sub_field('image');
+
+        ?>
+        <div class="card">
+          <div class="card__top">
+            <img src="<?php echo $image ?>"/>
+          </div>
+          <div class="card__bottom">
+            <h5><?php echo $title ; ?></h5>
+            <p class="minor"><?php echo $copy ; ?></p>
+          </div>
+        </div>
+
+
+
+          <?php endwhile; ?>
+      <?php endif; ?>
+
 
     </div>
-
-  
   </div>
 </section>
