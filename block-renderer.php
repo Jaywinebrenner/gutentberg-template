8550e8d327b44a511fa8f39c357853841c607e7c
teustice <teustice@msn.com>
2023-07-05 15:24:46 -0700

    finish testimonial

--- block-renderer.php
+++ block-renderer.php
@@ -140,6 +140,29 @@ function init_acf_fields()
                 'align' => array('full'),
             ),
             'keywords'          => array('testimonial', 'slider'),
+            'enqueue_assets' => function () {
+                wp_enqueue_script(
+                    'jquery',
+                    'https://code.jquery.com/jquery-3.7.0.min.js"
+                    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=',
+                    ['wp-blocks', 'wp-element', 'wp-components', 'wp-i18n'],
+                    22
+                );
+
+                wp_enqueue_script(
+                    'slick',
+                    'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js',
+                    ['wp-blocks', 'wp-element', 'wp-components', 'wp-i18n'],
+                    22
+                );
+
+                wp_enqueue_script(
+                    'testimonial',
+                    get_template_directory_uri() . '/assets/js/testimonial.js',
+                    ['wp-blocks', 'wp-element', 'wp-components', 'wp-i18n'],
+                    22
+                );
+            },
         ));
     }
 }
--- template-parts/block/testimonial.php
+++ template-parts/block/testimonial.php
@@ -10,21 +10,25 @@
 $id = 'testimonial-' . $block['id'];
 ?>
 
-<section id="<?php echo $id; ?>" class="testimonial container" style="background-color: <?php the_field('background'); ?>">
-    <p class="minor testimonial__title"><?php the_field('title'); ?></p>
+<section id="<?php echo $id; ?>" class="testimonial" style="background-color: <?php the_field('background'); ?>">
+    <div class="container">
+        <p class="minor testimonial__title"><?php the_field('title'); ?></p>
 
-    <?php if (have_rows('testimonials')) : ?>
-        <?php while (have_rows('testimonials')) : the_row();
-        ?>
-            <div class="testimonial__quote">
-                <h3><?php the_sub_field('quote'); ?></h3>
-                <p class="italic"><?php the_sub_field('name'); ?></p>
+        <?php if (have_rows('testimonials')) : ?>
+            <div class="testimonial__slides">
+                <?php while (have_rows('testimonials')) : the_row();
+                ?>
+                    <div class="testimonial__quote">
+                        <h3><?php the_sub_field('quote'); ?></h3>
+                        <p class="italic"><?php the_sub_field('name'); ?></p>
+                    </div>
+                <?php endwhile; ?>
             </div>
-        <?php endwhile; ?>
-    <?php endif; ?>
+        <?php endif; ?>
 
-    <?php $button = get_field('button'); ?>
-    <?php if ($button['url']) : ?>
-        <a href="<?php echo $button['url']; ?>" class="btn_red"><?php echo $button['text']; ?></a>
-    <?php endif; ?>
+        <?php $button = get_field('button'); ?>
+        <?php if ($button['url']) : ?>
+            <a href="<?php echo $button['url']; ?>" class="btn_red"><?php echo $button['text']; ?></a>
+        <?php endif; ?>
+    </div>
 </section>
