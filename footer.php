a810aafb5be871f433c9505dcba2bba81a595b24
Jay WInebrenner <jaywinebrenner@gmail.com>
2023-07-05 11:06:24 -0700

    wire up basic component

--- footer.php
+++ footer.php
@@ -1,6 +1,32 @@
 <footer id="colophon" class="site-footer wrap" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
 
-	<section class="footer-container">
+	<section class="footer-container footer container">
+		<div class="footer__top">
+			<div class="left">
+				<h4>
+				We’re a technology consulting firm that supplies strategic consultants, subject matter experts, and agile project teams to harness the power of both people and technology.
+				</h4>
+			</div>
+			<div class="right">
+				<div class="headquarters">
+					<h5>Headquarters</h5>
+					<p>Portland, Oregon</p>
+					<p>9115 SW Oleson Rd, Ste 105 Portland, OR 97223</p>
+					<p>503.726.4545</p>
+
+				</div>
+				<div class="explore">
+					<h5>Explore Mode</h5>
+					<p class="minor">Contact Us</p>
+					<p>Employee Login</p>
+					<div class="social-wrapper">
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="footer__top">
+		</div>
 
 	</section><!-- .site-info -->
 
