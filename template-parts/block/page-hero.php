0ae31b8983166ec2a620596926bfca47c225fa54
teustice <teustice@msn.com>
2023-07-06 12:56:31 -0700

    tweaks

--- template-parts/block/page-hero.php
+++ template-parts/block/page-hero.php
@@ -14,29 +14,26 @@ $id = 'page-hero-' . $block['id'];
   <div class="container">
     <div class="monitor-linen-bars"></div>
     <div class="page-hero-wrapper">
-        <div class="page-hero-wrapper__left">
-            <h1><?php the_field('title'); ?></h1>
-            <p><?php the_field('subtitle'); ?></p>
-            <?php
-            $button = get_field('button');
-            if( $button ): ?>
-            <div class="btn-wrapper">
-            <a href="<?php echo esc_url( $button['url'] ); ?>" class="btn_red"><?php echo $button['text']?></a>
-            </div>
-            <?php endif; ?>
-        </div>
+      <div class="page-hero-wrapper__left">
+        <h1><?php the_field('title'); ?></h1>
+        <p><?php the_field('subtitle'); ?></p>
+        <?php
+        $button = get_field('button');
+        if ($button) : ?>
+          <div class="btn-wrapper">
+            <a href="<?php echo esc_url($button['url']); ?>" class="btn_red"><?php echo $button['text'] ?></a>
+          </div>
+        <?php endif; ?>
+      </div>
 
-        <div class="page-hero-wrapper__right">
-        <?php if( get_field('image') ): ?>
-        <img 
-        class="desktop-image" 
-        src="<?php the_field('image'); ?>" 
-        />
+      <div class="page-hero-wrapper__right">
+        <?php if (get_field('image')) : ?>
+          <img class="desktop-image" src="<?php the_field('image'); ?>" />
         <?php endif; ?>
-        <?php if( get_field('mobile_image') ): ?>
-        <img class="mobile-image" src="<?php the_field('mobile_image'); ?>" />
+        <?php if (get_field('mobile_image')) : ?>
+          <img class="mobile-image" src="<?php the_field('mobile_image'); ?>" />
         <?php endif; ?>
-        </div>
+      </div>
     </div>
 
   </div>
