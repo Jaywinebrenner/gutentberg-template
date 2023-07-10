12d8e14f0386eae4f9c35659287248ce511fb253
teustice <teustice@msn.com>
2023-07-10 12:54:41 -0700

    tweaks

--- template-parts/block/our-services.php
+++ template-parts/block/our-services.php
@@ -17,54 +17,50 @@ $id = 'our-services-' . $block['id'];
             <div class="our-services__top">
                 <h2><?php the_field('main_title'); ?></h2>
                 <p><?php the_field('main_subtitle'); ?></p>
-                <?php if( get_field('image') ): ?>
+                <?php if (get_field('image')) : ?>
                     <div class="image-wrapper">
                         <img class="image" src="<?php the_field('image'); ?>" />
                     </div>
-                    <?php endif; ?>
+                <?php endif; ?>
 
-                    <?php if (get_field('mobile_image')) {?>
-                        <div class="mobile-image-wrapper">
-                            <img class="mobile-image" src="<?php the_field('mobile_image'); ?>" />
-                        </div>
-                    <?php } else { ?>
-                        <div class="image-wrapper">
-                            <img class="image" src="<?php the_field('image'); ?>" />
-                        </div>
-                    <?php } ?>
- 
+                <?php if (get_field('mobile_image')): ?>
+                    <div class="mobile-image-wrapper">
+                        <img class="mobile-image" src="<?php the_field('mobile_image'); ?>" />
+                    </div>
+                <?php endif; ?>
+
 
             </div>
 
             <div class="our-services__bottom">
-                
+
                 <div class="left">
                     <h3><?php the_field('left_column_title'); ?></h3>
                     <p><?php the_field('left_column_content'); ?></p>
-                </div>  
+                </div>
 
                 <div class="right">
 
-                <?php
+                    <?php
 
-                if( have_rows('dropdown') ):
-                    while( have_rows('dropdown') ) : the_row();
-                        $title = get_sub_field('title');
-                        $content = get_sub_field('content');
-                        ?>
-                        <div class="accordion-item">
-                            <div class="accordion-item__title">
-                                <h4><?php echo $title; ?></h4>
-                                <img class="chevron" src="<?php echo get_stylesheet_directory_uri();?>/assets/images/our-services/chevron.svg"/>
+                    if (have_rows('dropdown')) :
+                        while (have_rows('dropdown')) : the_row();
+                            $title = get_sub_field('title');
+                            $content = get_sub_field('content');
+                    ?>
+                            <div class="accordion-item">
+                                <div class="accordion-item__title">
+                                    <h4><?php echo $title; ?></h4>
+                                    <img class="chevron" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/our-services/chevron.svg" />
+                                </div>
+                                <div class="accordion-item__content">
+                                    <p><?php echo $content; ?></p>
+                                </div>
                             </div>
-                            <div class="accordion-item__content">
-                                <p><?php echo $content; ?></p>
-                            </div>
-                        </div>
-                        <?php
-                    endwhile;
-                endif;
-                ?>
+                    <?php
+                        endwhile;
+                    endif;
+                    ?>
 
                 </div>
             </div>
