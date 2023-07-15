b3c2cf7a18b18fcfddbfca673e900a0188878add
Jay WInebrenner <jaywinebrenner@gmail.com>
2023-07-15 10:28:34 -0700

    clean up

--- 404.php
+++ 404.php
@@ -14,7 +14,6 @@ get_header(); ?>
 				<h1>404</h1>
 				<h3>Oops! Looks like the page you’re looking for isn’t here. </h3>
 				<p>Redirecting you in [<span class="count">10</span>] seconds.</p>
-				<a href="<?php echo home_url(); ?>" class="btn_1">home</a>
 			</div>
 		</section><!-- .error-404 -->
 
