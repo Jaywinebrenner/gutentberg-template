5ae9d0000bbfdb15d5593d407523956fb91e7aff
teustice <teustice@msn.com>
2023-07-17 09:54:02 -0700

    add container

--- template-parts/block/inline-hero.php
+++ template-parts/block/inline-hero.php
@@ -10,13 +10,13 @@
 $id = 'inline-hero-' . $block['id'];
 ?>
 
-<section id="<?php echo $id; ?>" class="inline-hero">
-        <?php if (get_field('image')) : ?>
-          <div class="desktop-image parallax-wrapper">
-            <img src="<?php the_field('image'); ?>" alt="">
-          </div>
-        <?php endif; ?>
-        <div class="mobile-image parallax-wrapper">
-          <img src="<?php echo get_field('mobile_image') ? get_field('mobile_image') : get_field('image'); ?>" alt="">
-        </div>
+<section id="<?php echo $id; ?>" class="inline-hero container">
+  <?php if (get_field('image')) : ?>
+    <div class="desktop-image parallax-wrapper">
+      <img src="<?php the_field('image'); ?>" alt="">
+    </div>
+  <?php endif; ?>
+  <div class="mobile-image parallax-wrapper">
+    <img src="<?php echo get_field('mobile_image') ? get_field('mobile_image') : get_field('image'); ?>" alt="">
+  </div>
 </section>
