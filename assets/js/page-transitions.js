b1683e9d24ce77c2ce71247e08253bda065a8d7b
teustice <teustice@msn.com>
2023-07-17 14:18:58 -0700

    update sub menu

--- assets/js/page-transitions.js
+++ assets/js/page-transitions.js
@@ -2,12 +2,4 @@ document.addEventListener("DOMContentLoaded", () => {
   setTimeout(function () {
     document.body.classList.remove('fade');
   }, 230);
-
-  [...document.querySelectorAll('a')].forEach((el) => {
-    if(el.href != '#') {
-      el.addEventListener('click', () => {
-        document.body.classList.add('fade')
-      });
-    }
-  })
 });
