b009064978ee67ceabf9b301d992472d7cfd6b9c
teustice <teustice@msn.com>
2023-07-17 10:48:49 -0700

    tweaks

--- assets/js/error-404.js
+++ assets/js/error-404.js
@@ -1,20 +1,23 @@
 class Error {
-    constructor(el) {
-      this.el = el;
-      this.init();
+    constructor() {
+        this.init();
     }
     init() {
         let el = document.querySelector('.count'),
-        total = el.innerHTML,
-        timeinterval = setInterval(function () {
-            total = --total;
-            el.textContent = total;
-            if (total <= 0) {
-                clearInterval(timeinterval);
-                window.location = '/';
-            }
-        }, 1000);
+            total = el.innerHTML,
+            timeinterval = setInterval(function () {
+                total = --total;
+                el.textContent = total;
+                if (total <= 0) {
+                    clearInterval(timeinterval);
+                    window.location = '/';
+                }
+            }, 1000);
     }
 }
-  
-  document.addEventListener('DOMContentLoaded', () => new Error());
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector('.error-404')) {
+        new Error()
+    }
+});
