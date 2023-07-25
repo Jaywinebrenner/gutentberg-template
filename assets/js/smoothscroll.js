a8b673fcb8300142643ae850c68e4bfe03259f3e
Jay WInebrenner <jaywinebrenner@gmail.com>
2023-07-25 15:07:06 -0700

    update smooth scroll js

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -4,13 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
   //only load smooth scroll on front end
   if (!document.body.classList.contains('wp-admin')) {
     const lenis = new Lenis()
+    const lenisModal = new Lenis({
+      content: document.querySelector('.modal-container')
+    })
 
     lenis.on('scroll', (e) => {
+      // console.log(e)
+    })
+    lenisModal.on('scroll', (e) => {
       // console.log(e)
     })
 
     function raf(time) {
       lenis.raf(time)
+      lenisModal.raf(time)
       requestAnimationFrame(raf)
     }
 
