7c7eb7b9f3047f11cb0d596259608cc46d160a8e
teustice <teustice@msn.com>
2023-08-28 15:22:07 -0700

    add hotjar

--- header.php
+++ header.php
@@ -56,6 +56,24 @@
 			})();
 		}
 	</script>
+
+	<!-- Hotjar Tracking Code for CorSource -->
+	<script>
+		(function(h, o, t, j, a, r) {
+			h.hj = h.hj || function() {
+				(h.hj.q = h.hj.q || []).push(arguments)
+			};
+			h._hjSettings = {
+				hjid: 3631324,
+				hjsv: 6
+			};
+			a = o.getElementsByTagName('head')[0];
+			r = o.createElement('script');
+			r.async = 1;
+			r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+			a.appendChild(r);
+		})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+	</script>
 	<!-- END Tracking scripts -->
 
 	<?php wp_head(); ?>
