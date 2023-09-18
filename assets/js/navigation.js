3124bf637624a688b3e1143fc7c0fe07b5a52a33
Jay WInebrenner <jaywinebrenner@gmail.com>
2023-09-18 16:15:12 -0700

    wire up basic mobile nav animation

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -12,7 +12,6 @@ class Navigation {
 
 		menuItems.forEach(function(item) {
 			var link = item.querySelector('a');
-			console.log("link", link.href)
 			if (link && link.href === currentPageURL) {
 				item.classList.add('active');
 			}
@@ -32,11 +31,63 @@ class Navigation {
 		var hamburger = document.getElementById('myBtn');
 		var xImage = document.querySelector('.x-img');
 		var navMenuMobile = document.querySelector('.navigation-menu-mobile');
+		var body = document.body;
+
+		function reset() {
+			console.log("reset")
+			// navMenuMobile.classList.remove('reset');
+			// navMenuMobile.style.display = 'none;'
+			// navMenuMobile.style.transform = 'translate(100%, -107px)'
+			setTimeout(() => {
+				navMenuMobile.style.background = 'red;'
+			}, 300);
+		}
+	
+		function disableScroll() {
+			navMenuMobile.style.transform = 'translate(0, -107px)';
+			body.style.overflow = 'hidden';
+			navMenuMobile.style.visiblity = 'visible';
+		}
+	
+		function enableScroll() {
+			console.log("enableScroll")
+			body.style.overflow = 'auto';
+			navMenuMobile.style.visiblity = 'hidden';
+			setTimeout(function() {
+
+				setTimeout(function() {
+					navMenuMobile.style.transform = 'translate(-100%, -107px)';
+					navMenuMobile.style.visiblity = 'visible';
+				}, 300);
+			}, 300);
+			
+		}
+	
 		hamburger.addEventListener('click', function() {
 			navMenuMobile.classList.toggle('open');
+	
+			if (navMenuMobile.classList.contains('open')) {
+				disableScroll();
+				// navMenuMobile.style.transform = 'translate(0, -107px)';
+			} else {
+				enableScroll();
+				navMenuMobile.style.transform = 'translate(100%, -107px)';
+				reset();
+			}
 		});
+	
+		// Add a click event listener to the X image
 		xImage.addEventListener('click', function() {
-			navMenuMobile.classList.remove('open');
+			// Slide out the navigation-menu-mobile to the right
+			navMenuMobile.style.transform = 'translate(100%, -107px)';
+			
+			// Enable scrolling when the menu is closed
+			enableScroll();
+			
+			// Remove the 'open' class after sliding out
+			setTimeout(function() {
+				navMenuMobile.classList.remove('open');
+			}, 300); // Adjust the time to match your transition duration
 		});
 
 
