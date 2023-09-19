e18b38aad1a36c056130b0990e4a4ec40cb83b09
Jay WInebrenner <jaywinebrenner@gmail.com>
2023-09-19 08:58:57 -0700

    crack the sliding out nut

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -5,68 +5,66 @@ class Navigation {
 	init() {
 
 		//CHANGE COLOR OF DESKTOP NAV ITEM COLOR IF ACTIVE
-		var menuItems = document.querySelectorAll('.navigation-menu li');
-		var menuItemsMobile = document.querySelectorAll('.menu li');
-
-		var currentPageURL = window.location.href;
+		let menuItems = document.querySelectorAll('.navigation-menu li');
+		let menuItemsMobile = document.querySelectorAll('.menu li');
+		let currentPageURL = window.location.href;
 
 		menuItems.forEach(function(item) {
-			var link = item.querySelector('a');
+			let link = item.querySelector('a');
 			if (link && link.href === currentPageURL) {
 				item.classList.add('active');
 			}
 		});
 
 		menuItemsMobile.forEach(function(item) {
-			var link = item.querySelector('a');
+			let link = item.querySelector('a');
 			console.log("link", link.href)
 			if (link && link.href === currentPageURL) {
 				item.classList.add('mobi-active');
 			}
 		});
 
-
-
 		// SLIDE MOBILE NAVIGATION MENU IN FROM LEFT AND HAVE IT SLIDE OUT WHEN CLICKING THE X
-		var hamburger = document.getElementById('myBtn');
-		var xImage = document.querySelector('.x-img');
-		var navMenuMobile = document.querySelector('.navigation-menu-mobile');
-		var body = document.body;
-
+		let hamburger = document.getElementById('myBtn');
+		let xImage = document.querySelector('.x-img');
+		let navMenuMobile = document.querySelector('.navigation-menu-mobile');
+		let body = document.body;
 		function disableScroll() {
-			navMenuMobile.style.transform = 'translate(0, -107px)';
 			body.style.overflow = 'hidden';
-			navMenuMobile.style.visiblity = 'visible';
+			document.documentElement.style.overflow = 'hidden';
+			document.documentElement.style.height = '100%';
 		}
 	
 		function enableScroll() {
-			console.log("enableScroll")
 			body.style.overflow = 'auto';
-			navMenuMobile.style.visiblity = 'hidden';
-			setTimeout(function() {
-
-				setTimeout(function() {
-					navMenuMobile.style.transform = 'translate(-100%, -107px)';
-					navMenuMobile.style.visiblity = 'visible';
-				}, 300);
-			}, 300);
-			
+			document.documentElement.style.overflow = 'visible';
+			document.documentElement.style.height = 'unset';
+			const allLis = document.querySelectorAll("li");
+			allLis.forEach(function(li) {
+				li.style.visibility = 'hidden';
+			});
+			setTimeout(() => {
+				navMenuMobile.style.visibility = 'hidden';
+				navMenuMobile.style.transform = 'translate(-80%, -107px)';
+			}, 500);
 		}
 	
 		hamburger.addEventListener('click', function() {
-			navMenuMobile.classList.toggle('open');
-	
+			const allLis = document.querySelectorAll("li");
+			navMenuMobile.classList.add('open');
+			navMenuMobile.style.visibility = 'visible';
+			navMenuMobile.style.transform = 'translate(0, -107px)';
+			allLis.forEach(function(li) {
+				li.style.visibility = 'visible';
+			});
 			if (navMenuMobile.classList.contains('open')) {
 				disableScroll();
-			} else {
-				enableScroll();
-				navMenuMobile.style.transform = 'translate(100%, -107px)';
-				reset();
-			}
+			} 
 		});
 	
 		// Add a click event listener to the X image
 		xImage.addEventListener('click', function() {
+			navMenuMobile.classList.remove('open');
 			navMenuMobile.style.transform = 'translate(100%, -107px)';
 			enableScroll();
 			setTimeout(function() {
